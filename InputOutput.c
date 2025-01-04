a1ed1c081451fa7177f55d9e26bf86d19a454846
Brayan Santos <153335608+Bsantosc@users.noreply.github.com>
2025-01-04 13:56:07 -0500

    Create InputOutput.c

---
+++ InputOutput.c
@@ -0,0 +1,21 @@
+//
+//  InputOutput.C
+//  learning
+//
+//  Created by Brayan Santos on 1/4/25.
+//
+// Basic Input output program
+
+#include <stdio.h>
+
+int main(){
+    
+    char name[100];
+    
+    printf("Print your name \n");
+    scanf("%s", name);
+    
+    printf("Your name is: %s\n", name );
+    
+    return 0;
+}
